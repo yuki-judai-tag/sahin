13b4adf63af5ea69f3b95aa75f8b9f3606bb8ff2
Şahin Topaç <topacsahin26@gmail.com>
2025-04-29 09:52:08 +0300

    ilk update

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,9 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
     });
 }
 
