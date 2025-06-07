be2814ccee9131ee646339d590c0511fba7a8ca3
Şahin Topaç <topacsahin26@gmail.com>
2025-06-07 18:02:29 +0300

    final update

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";  // CORS modülünü ekledik
+import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";  // <--- burayı ekle
 import { connectionDB } from "./config/db.js";
 import productRoutes from "../routes/product.route.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log("__dirname:", __dirname);
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// CORS Middleware'ini kullan
-app.use(cors());  // Tüm domainlere izin verir (Güvenli değil ama lokal geliştirme için yeterli)
-const __dirname = path.resolve(); // __dirname'i tanımladık
-// JSON Middleware
+// CORS Middleware
+app.use(cors());
 app.use(express.json());
 
-// API Route'ları
+// API Routes
 app.use("/api/products", productRoutes);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, ".", "frontend", "dist")));
+    app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, ".", "frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "..", "frontend", "dist", "index.html"));
     });
 }
 
-// Server'ı başlat
 app.listen(PORT, () => {
     connectionDB();
     console.log("Server started at http://localhost:" + PORT);
