5922e4b98edc88257724396c58c6f73aef42a66c
Şahin Topaç <topacsahin26@gmail.com>
2025-06-07 17:42:47 +0300

    final update

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "./frontend/dist")));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
